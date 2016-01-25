f150980694dc43a0b5f617b1f84342e634cb490a
Lakret <Rakuretu@gmail.com>
2016-01-25 20:20:41 +0300

    assembly

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,7 +1,16 @@
 package main
 
+import org.apache.spark._
+import com.datastax.spark.connector._
 
 object Middleware extends App  {
+// bin/spark-shell --packages datastax:spark-cassandra-connector:1.5.0-RC1-s_2.10 --master spark://ip-172-31-57-38:7077 --driver-java-options spark.driver.allowMultipleContexts=true
+
+  val conf = new SparkConf(true).set("spark.cassandra.connection.host", "172.31.57.38").set("spark.driver.allowMultipleContexts", "true")
+  val sc = new SparkContext("spark://ip-172-31-57-38:7077", "text", conf)
+  val airlines = sc.cassandraTable("testairlines", "airlines")
 
   println("running...")
+
+  println(airlines.first)
 }
