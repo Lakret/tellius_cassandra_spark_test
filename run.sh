7f21a784b69b4ae76624dc3ccf8e6c3a750b9a6b
Lakret <Rakuretu@gmail.com>
2016-01-30 04:54:08 +0300

    spark

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 sbt assembly
-~/spark/spark-1.5.2-bin-hadoop2.6/bin/spark-submit --class main.Application --master spark://ip-172-31-57-38:7077 --conf "spark.executor.memory=2g" ~/tellius_cassandra_spark_test/target/scala-2.10/tellius_cassandra_spark_test-assembly-0.0.1.jar
+~/spark/spark-1.5.2-bin-hadoop2.6/bin/spark-submit --class main.Application --master spark://ip-172-31-58-106:7077 --conf "spark.executor.memory=3g" ~/tellius_cassandra_spark_test/target/scala-2.10/tellius_cassandra_spark_test-assembly-0.0.1.jar
